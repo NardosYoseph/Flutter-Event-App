4a1f7f1e1b4075e4b63a4ed067a32e29c7cba527
NardosYosef <nardosyosef123@gmail.com>
2024-02-08 10:27:40 +0300

    home page

--- lib/app/pages/home_page.dart
+++ lib/app/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:event_app/app/utils/text_util.dart';
+import 'package:event_app/app/widgets/common/card.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
@@ -6,13 +7,50 @@ import 'package:get/get.dart';
 import 'package:event_app/app/controllers/auth_controller.dart';
 
 class HomePage extends StatelessWidget {
- HomePage({super.key});
- final AuthController controller = Get.find();
+  HomePage({super.key});
+  final AuthController controller = Get.find();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-appBar: AppBar(title: TextUtil(text: "hello",color: Colors.black,),),
-body: Container(child: Text("home"),),
-    );
+      appBar: AppBar(),
+        body: Padding(
+          
+          padding: const EdgeInsets.all(15),
+          child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+         
+          TextUtil(
+            text: "Popular Events",
+            color: Colors.black,
+            size: 16,
+          ),
+          const SizedBox(
+            height: 15,
+          ),
+          const SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+          //         const SizedBox(
+          //   width: 15,
+          // ),
+                MyCard(),
+                SizedBox(
+                  width: 10,
+                ),
+                MyCard(),
+                SizedBox(
+                  width: 10,
+                ),
+                MyCard(),
+              ],
+            ),
+          )
+                ],
+              ),
+        ));
   }
 }
--- lib/app/widgets/common/card.dart
+++ lib/app/widgets/common/card.dart
@@ -8,7 +8,6 @@ class MyCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return Card(
       elevation: 10,
-
       child: Stack(
         alignment: AlignmentDirectional.bottomStart,
         children:[ 
@@ -16,12 +15,22 @@ class MyCard extends StatelessWidget {
         height: 200,
         width: 200,
         child: Image.asset("assets/event1.jpg",fit: BoxFit.cover,),
-
     ),
-    TextUtil(text: "AMharaBank First Year aniver"),
-    Row(children: [
-      Icon(Icons.star,color: Color.fromARGB(255, 245, 221, 3),)
+    Column(
+      children: [
+        TextUtil(text: "AMharaBank First\n Year aniversary"),
+     
+    Row(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+      Icon(Icons.star,color: Color.fromARGB(255, 245, 189, 3),),
+      Icon(Icons.star,color: Color.fromARGB(255, 245, 189, 3),),
+      Icon(Icons.star,color: Color.fromARGB(255, 245, 189, 3),),
+      Icon(Icons.star,color: Color.fromARGB(255, 245, 189, 3),),
+      Icon(Icons.star,color: Color.fromARGB(255, 245, 189, 3),),
     ],)
+     ],
+    ),
     ]),
     );
   }
